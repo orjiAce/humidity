eb953b8456a0e175f4536e389b5c4b1907fdabc4
Orji Joseph <orjiace@gmail.com>
2020-07-10 19:17:05 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter, Route, Switch} from "react-router-dom"
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="App">
-<h1>hey</h1>
+<BrowserRouter>
+  <Switch>
+    <Route exact path='/' component={Home}/>
+  </Switch>
+</BrowserRouter>
     </div>
   );
 }
